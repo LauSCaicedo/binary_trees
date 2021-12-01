880b76db157f0223b863453dc9a35ec7938682fc
LauSCaicedo <3383@holbertonschool.com>
2021-12-01 12:26:36 -0500

    Is full

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,13 +10,35 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int h_left, h_right;
+	int h_left, h_right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+
+	return (h_left - h_right);
+}
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int h_left, h_right = 0;
 
 	if (tree == NULL)
 		return (-1);
 
-	h_left = binary_tree_balance(tree->left) + 1;
-	h_right = binary_tree_balance(tree->right) + 1;
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
 
-	return (h_left - h_right);
+	if (h_left > h_right)
+		return (h_left + 1);
+	else
+		return (h_right + 1);
 }
