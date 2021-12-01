96dad941595e8d316ba74476c27268b53e61614b
mayii527 <3434@holbertonschool.com>
2021-12-01 11:38:12 -0500

    fixs

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int h_left, h_right = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
+	if (tree == NULL)
+		return (-1);
 
 	h_left = binary_tree_height(tree->left);
 	h_right = binary_tree_height(tree->right);
 
-	if (h_left < h_right)
+	if (h_left > h_right)
 		return (h_left + 1);
 	else
 		return (h_right + 1);
